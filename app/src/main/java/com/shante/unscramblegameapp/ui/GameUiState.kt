54aa9d323cb3f15c34fb9f75cd4eb318e397f816
Alex <alexsid.ShanTE@gmail.com>
2022-12-19 19:08:38 +0300

    Added viewmodel

--- app/src/main/java/com/shante/unscramblegameapp/ui/GameUiState.kt
+++ app/src/main/java/com/shante/unscramblegameapp/ui/GameUiState.kt
@@ -1,4 +1,9 @@
 package com.shante.unscramblegameapp.ui
 
-class GameUiState {
-}
+data class GameUiState(
+    val currentScrambledWord: String = "",
+    val isGuessedWordWrong: Boolean = false,
+    val currentWordCount: Int = 0,
+    val score: Int = 0,
+    val isGameOver: Boolean = false
+)
